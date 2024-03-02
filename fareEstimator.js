673c9e301d7651114921cd7e2b6670c5579a7da7
David Q <dvdquerol@gmail.com>
2024-03-02 17:15:05 +0100

    Add fareEstimator.js and remove main.js

---
+++ fareEstimator.js
@@ -0,0 +1,32 @@
+/*
+
+Uber is building a Fare Estimator that can tell you how much your ride will cost before you request it. It works by passing approximated ride distance and ride time through this formula:
+(Cost per minute) * (ride time) + (Cost per mile) * (ride distance)
+
+where Cost per minute and Cost per mile depend on the type of car.
+
+You are one of the engineers building the Fare Estimator, so knowing cost per minute and cost per mile for each car type, as well as ride distance and ride time, return the fare estimates for all car types.
+
+Example
+
+For ride_time = 30, ride_distance = 7, cost_per_minute = [0.2, 0.35, 0.4, 0.45] and cost_per_mile = [1.1, 1.8, 2.3, 3.5], the output should be
+fareEstimator(ride_time, ride_distance, cost_per_minute, cost_per_mile) = [13.7, 23.1, 28.1, 38].
+*/
+
+RideTime = 30;
+RideDistance = 7;
+CostPerMinute = [0.2, 0.35, 0.4, 0.45];
+CostPerMile = [1.1, 1.8, 2.3, 3.5];
+
+function solution(RideTime, RideDistance, CostPerMinute, CostPerMile)  {
+
+    // Fórmula: (cost_per_minute) * (ride_time) + (cost_per_mile) * (ride_distance)
+    // Cada índice del array representa un vehículo diferente.
+
+    return CostPerMinute.map((costMinute, index) => {
+        const costMile = CostPerMile[index]
+        return costMinute * RideTime + costMile * RideDistance
+    })
+}
+
+console.log(solution(RideTime, RideDistance, CostPerMinute, CostPerMile));
